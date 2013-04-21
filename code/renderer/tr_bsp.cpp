21a15843f64d08330a469e4f849a4e37f4cfdbdf
Jonathan Gray <jsg@jsg.id.au>
2013-04-21 20:21:29 +1000

    fix some accesses beyond the end of arrays spotted via clang

--- code/renderer/tr_bsp.cpp
+++ code/renderer/tr_bsp.cpp
@@ -1095,7 +1095,6 @@ static	void R_LoadFogs( lump_t *l, lump_t *brushesLump, lump_t *sidesLump, world
 			out->parms.color[0] = 1.0f;
 			out->parms.color[1] = 0.0f;
 			out->parms.color[2] = 0.0f;
-			out->parms.color[3] = 0.0f;
 			out->parms.depthForOpaque = 250.0f;
 		}
 		else
@@ -1135,7 +1134,6 @@ static	void R_LoadFogs( lump_t *l, lump_t *brushesLump, lump_t *sidesLump, world
 		out->parms.color[0] = 0.0f;
 		out->parms.color[1] = 0.0f;
 		out->parms.color[2] = 0.0f;
-		out->parms.color[3] = 0.0f;
 		out->parms.depthForOpaque = 0.0f;
 		out->colorInt = 0x00000000;
 		out->tcScale = 0.0f;
