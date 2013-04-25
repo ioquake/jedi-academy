b1f71d5b81e7ce1cdcfcc3cf5017213255f0eaf4
Jonathan Gray <jsg@jsg.id.au>
2013-04-26 00:08:03 +1000

    don't try to compile c files as c++

--- codemp/game/g_main.c
+++ codemp/game/g_main.c
@@ -509,9 +509,6 @@ This must be the very first function compiled into the .q3vm file
 ================
 */
 #include "../namespace_begin.h"
-#ifdef __linux__
-extern "C" {
-#endif
 int vmMain( int command, int arg0, int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11  ) {
 	switch ( command ) {
 	case GAME_INIT:
@@ -694,9 +691,6 @@ int vmMain( int command, int arg0, int arg1, int arg2, int arg3, int arg4, int a
 
 	return -1;
 }
-#ifdef __linux__
-}
-#endif
 #include "../namespace_end.h"
 
 
